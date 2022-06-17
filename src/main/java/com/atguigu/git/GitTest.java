b5733598794e88ab781842335217ecfa1076251c
Volvo <641109416@qq.com>
2022-06-18 02:03:58 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,8 @@ public class GitTest {
         System.out.println("Hello github!");
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
+        System.out.println("hot-fix test!");
         System.out.println("master test!");
+        System.out.println("push test!");
     }
 }
